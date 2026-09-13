b2f8be8a94f07104b0348a80d12ffbeada197435
maihuhero369 <mai.hu.hero.369@gmail.com>
2026-09-14 03:28:44 +0530

    Question 7

--- practice.07.py
+++ practice.07.py
@@ -1,9 +1,19 @@
-n = int(input("Enter number : "))
-a = n
-for i in range(a,0,-1):
-    for j in range(2*n):
-        if (j >= n - i and j <= n + i):
-            print("*" , end= " ")
-        else :
-            print(" " ,end= " ")   
-    print()    
+n = int(input("Enter n: "))
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end="")
+    for j in range(2 * (n - i)):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
+
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    for j in range(2 * (n - i)):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end="")
+    print()
