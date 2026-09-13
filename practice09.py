255f702ccb7be66c09ab52ee5bd7181475cb39f0
maihuhero369 <mai.hu.hero.369@gmail.com>
2026-09-14 03:32:53 +0530

    Question 9

---
+++ practice09.py
@@ -0,0 +1,13 @@
+n = int(input("Enter n: "))
+
+for i in range(n, 0, -1):
+    for j in range(n - i):
+        print(" ", end="")
+
+    for j in range(2 * i - 1):
+        if i == n or j == 0 or j == 2 * i - 2 or i == 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+
+    print()
